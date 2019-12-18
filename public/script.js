1889afc0daa082194f082a703b6968276b9db678
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-17 22:16:18 -0500

    minor

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ var w = 500,
 var colorscale = d3.scale.category10();
 
 //Legend titles
-var LegendOptions = ['Smartphone','Tablet'];
+var LegendOptions = ['Project 1','Project 2'];
 
 //Data
 var d = [
@@ -16,15 +16,7 @@ var d = [
 			{axis:"Search Engine",value:0.48},
 			{axis:"View Shopping sites",value:0.14},
 			{axis:"Paying Online",value:0.11},
-			{axis:"Buy Online",value:0.05},
-			{axis:"Stream Music",value:0.07},
-			{axis:"Online Gaming",value:0.12},
-			{axis:"Navigation",value:0.27},
-			{axis:"App connected to TV program",value:0.03},
-			{axis:"Offline Gaming",value:0.12},
-			{axis:"Photo Video",value:0.4},
-			{axis:"Reading",value:0.03},
-			{axis:"Listen Music",value:0.22}
+			{axis:"Buy Online",value:0.05}
 		  ],[
 			{axis:"Email",value:0.48},
 			{axis:"Social Networks",value:0.41},
@@ -33,15 +25,7 @@ var d = [
 			{axis:"Search Engine",value:0.46},
 			{axis:"View Shopping sites",value:0.29},
 			{axis:"Paying Online",value:0.11},
-			{axis:"Buy Online",value:0.14},
-			{axis:"Stream Music",value:0.05},
-			{axis:"Online Gaming",value:0.19},
-			{axis:"Navigation",value:0.14},
-			{axis:"App connected to TV program",value:0.06},
-			{axis:"Offline Gaming",value:0.24},
-			{axis:"Photo Video",value:0.17},
-			{axis:"Reading",value:0.15},
-			{axis:"Listen Music",value:0.12}
+			{axis:"Buy Online",value:0.14}
 		  ]
 		];
 
